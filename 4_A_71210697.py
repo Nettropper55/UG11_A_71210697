70e7145f1b50cafc9d9bd4c963373592dc094ca3
Nettropper55 <93307801+Nettropper55@users.noreply.github.com>
2021-11-26 06:52:59 +0700

    Update 4_A_71210697.py

--- 4_A_71210697.py
+++ 4_A_71210697.py
@@ -1,9 +1,9 @@
-def ambildanbalik(kalimat,awal,akhir,AbC):
-    if AbC == True:
-        Result = kalimat[akhir-1:awal-2:-1]
+def ambildanbalik(kalimat,awal,akhir,ABC):
+    if ABC == True:
+        Hasil = kalimat[akhir-1:awal-2:-1]
     else:
-        Result = kalimat[awal-1:akhir]
-    return Result
+        Hasil = kalimat[awal-1:akhir]
+    return Hasil
 print(ambildanbalik("abcdefg",2,4,True))
 print(ambildanbalik("abcdefg",1,5,False))
 print(ambildanbalik("Qwerty",3,4,True))
